00ce7009e3ac3957727cd118a130a986001c54db
ftomi <fehert@kkszki.hu>
2024-09-17 12:02:30 +0200

    3.feladat

--- employers/employers/Program.cs
+++ employers/employers/Program.cs
@@ -12,7 +12,16 @@ namespace employers
         static void Main(string[] args)
         {
             string[] fajl = File.ReadAllLines("tulajdonsagok_100sor.txt");
-
+            List<dolgozo> tulajdonsaglista = new List<dolgozo>();
+            foreach (string sor in fajl)
+            {
+                tulajdonsaglista.Add(new dolgozo(sor));  
+            }
+            foreach (var b in tulajdonsaglista)
+            {
+                Console.Write(b.nev +'\n');
+            }
+            Console.ReadLine();
         }
     }
 }
